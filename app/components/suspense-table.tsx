71bd5d7bcf8b27d256af525f17413b49718e4438
Steve Laing <steve.laing@gmail.com>
2023-09-18 19:16:02 +0100

    Add new row

--- app/components/suspense-table.tsx
+++ app/components/suspense-table.tsx
@@ -5,6 +5,8 @@ import "ka-table/style.css";
 import React, { useState } from 'react';
 
 import { ITableProps, kaReducer, Table } from 'ka-table';
+import { ICellEditorProps, IHeadCellProps } from 'ka-table/props';
+import { hideNewRow, saveNewRow, showNewRow } from 'ka-table/actionCreators';
 import { DataType, EditingMode, SortingMode } from 'ka-table/enums';
 import { DispatchFunc } from 'ka-table/types';
 
@@ -35,10 +37,60 @@ const columns = [
   { key: 'shockHsc', title: 'Shock HSC', dataType: DataType.String },
   { key: 'shockLsr', title: 'Shock LSR', dataType: DataType.String },
   { key: 'shockHsr', title: 'Shock HSR', dataType: DataType.String },
+  { key: 'addColumn', style: {width: 53}},
 ];
 
 const SuspenseTable: React.FC = () => {
   const [tableData, setTableData] = useLocalStorage(LOCAL_STORAGE_KEY, [blankRow]);
+
+  let maxValue = Math.max(...tableData.map(i => i.id));
+  const generateNewId = () => {
+    maxValue++;
+    return maxValue;
+  };
+  const AddButton: React.FC<IHeadCellProps> = ({
+    dispatch,
+  }) => {
+   return (
+    <div className='plus-cell-button'>
+      <img
+        src='static/icons/plus.svg'
+        alt='Add New Row'
+        title='Add New Row'
+        onClick={() => dispatch(showNewRow())}
+      />
+    </div>
+   );
+  };
+
+  const SaveButton: React.FC<ICellEditorProps> = ({
+    dispatch
+  }) => {
+    const saveNewData = () => {
+      const rowKeyValue = generateNewId();
+      dispatch(saveNewRow(rowKeyValue, {
+        validate: true
+      }));
+    };
+    return (
+     <div className='justify-center flex cursor-pointer'>
+      <img
+        src='static/icons/save.svg'
+        className='mr-15'
+        alt='Save'
+        title='Save'
+        onClick={saveNewData}
+      />
+      <img
+        src='static/icons/close.svg'
+        className='close-cell-button'
+        alt='Cancel'
+        title='Cancel'
+        onClick={() => dispatch(hideNewRow())}
+      />
+     </div>
+   );
+  };
 
   const tablePropsInit: ITableProps = {
     columns: columns,
@@ -61,6 +113,22 @@ const SuspenseTable: React.FC = () => {
     <Table
       {...tableProps} // ka-table UI is rendered according to props
       dispatch={dispatch} // dispatch is required for obtain new actions from the UI
+      childComponents={{
+        cellEditor: {
+          content: (props) => {
+            if (props.column.key === 'addColumn'){
+              return <SaveButton {...props}/>;
+            }
+          }
+        },
+        headCell: {
+          content: (props) => {
+            if (props.column.key === 'addColumn'){
+              return <AddButton {...props}/>;
+            }
+          }
+        }
+      }}
     />
   );
 };
