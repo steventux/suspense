ab84be724326abd77320ba194e1525d2997ca976
Steve Laing <steve.laing@gmail.com>
2023-10-03 15:28:46 +0100

    Remove vertical spacing between heading and table

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ const SuspenseTable = dynamic(() => import('./components/suspense-table'), { ssr
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="z-10 max-w-10xl w-full items-center font-mono text-sm lg:flex">
         <Title text="Suspense" description="Keep track of your settings" />
       </div>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-10xl w-full items-center font-mono text-sm lg:flex">
         <SuspenseTable />
       </div>
     </main>
