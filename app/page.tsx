55b92e8d2fae86229bbdd702d94381d7db696bec
Steve Laing <steve.laing@gmail.com>
2023-10-04 10:53:56 +0100

    Make strapline consistent

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="z-10 max-w-10xl w-full items-center font-mono text-sm lg:flex">
-        <Title text="Suspense" description="Keep track of your settings" />
+        <Title text="Suspense" description="Record your suspension settings" />
       </div>
       <div className="z-10 max-w-10xl w-full items-center font-mono text-sm lg:flex">
         <SuspenseTable />
