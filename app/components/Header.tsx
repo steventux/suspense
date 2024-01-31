9e900af4bd1338316ff9447b61acef0f47f6c60b
Steve Laing <steve.laing@gmail.com>
2024-01-31 13:19:14 +0000

    Refactor layout and other frontend components
    Simplify and refactor profiles to work in a standard CRUD pattern using
    the underlying profiles API and CRUD actions.

---
+++ app/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='w-full m-auto text-center p-4'>
+      <h1 className='font-bold text-4xl'>Suspense</h1>
+      <p className='text-sm'>Record your suspension settings</p>
+    </div>
+  )
+}
