fe76bdc4c99fb776d5565d93b2255938e3d18916
Steve Laing <steve.laing@gmail.com>
2024-02-21 13:17:53 +0000

    Add fieldValue attribute

--- app/components/form/LabelAndTextField.tsx
+++ app/components/form/LabelAndTextField.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import { SyntheticEvent } from 'react'
 import Label from './Label'
 import TextField from './TextField'
 
 interface LabelAndTextFieldProps extends React.HTMLAttributes<any> {
   fieldName: string;
+  fieldValue: string;
   fieldSetter: React.Dispatch<React.SetStateAction<string>>;
   labelText: string;
 }
 
-export default function LabelAndTextField({fieldName, fieldSetter, labelText}: LabelAndTextFieldProps) {
+export default function LabelAndTextField({fieldName, fieldValue, fieldSetter, labelText}: LabelAndTextFieldProps) {
   return (
     <div className="md:flex md:items-center mb-6">
       <div className="md:w-1/4">
         <Label fieldName={fieldName} labelText={labelText} />
       </div>
       <div className="md:w-2/4">
-        <TextField fieldName={fieldName} fieldSetter={fieldSetter} />
+        <TextField fieldName={fieldName} fieldSetter={fieldSetter} fieldValue={fieldValue} />
       </div>
     </div>
   )
--- app/components/form/TextField.tsx
+++ app/components/form/TextField.tsx
@@ -3,10 +3,11 @@ import { SyntheticEvent } from 'react'
 
 interface TextFieldProps extends React.HTMLAttributes<any> {
   fieldName: string;
+  fieldValue: string;
   fieldSetter: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function TextField({fieldName, fieldSetter}: TextFieldProps) {
+export default function TextField({fieldName, fieldValue, fieldSetter}: TextFieldProps) {
   const classNames = 'bg-gray-200 appearance-none border-2 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500';
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
@@ -14,6 +15,6 @@ export default function TextField({fieldName, fieldSetter}: TextFieldProps) {
   }
 
   return (
-    <input className={classNames} id={fieldName} type="text" onChange={onChange}/>
+    <input className={classNames} id={fieldName} value={fieldValue} type="text" onChange={onChange}/>
   )
 }
